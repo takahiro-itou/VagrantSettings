e50081c4a81cbf0b4563d00e186f9483866856fb
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2018-04-01 20:35:15 +0900

    設定を追加
    (1) エイリアスの設定を追加

---
+++ Bash/AliasBasic.sh
@@ -0,0 +1,19 @@
+
+##
+##  基本的なエイリアスの設定。
+##
+##  @file   Bash/AliasBasic.sh
+##
+##  Copyright (C) 2014/07,  Takahiro Itou.
+##  All Rights Reserved.
+##
+
+alias  ll='ls -l --color=tty'
+alias  la='ls -a --color=tty'
+alias  ls='ls --color=tty'
+
+alias  rm='rm -i'
+alias  view='vim -R '
+
+##echo  "Load  ${HOME}/Settings/Bash/AliasBasic.sh : OK"  1>&2
+
--- Bash/BashAlias.sh
+++ Bash/BashAlias.sh
@@ -1,5 +1,15 @@
 
+##
+##  エイリアスの設定。
+##
+##  @file   Bash/BashAlias.sh
+##
+##  Copyright (C) 2014/07,  Takahiro Itou.
+##  All Rights Reserved.
+##
+
 if [ -f ~/Settings/Bash/AliasBasic.sh ] ; then
   source  ~/Settings/Bash/AliasBasic.sh
 fi
+echo  "Load  ${HOME}/Settings/Bash/BashAlias.sh : OK"  1>&2
 
