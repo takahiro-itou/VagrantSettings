b6dce5a0bccf5a64c8f9a209c08b1a003e1ee9c3
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2018-04-01 20:44:48 +0900

    キーバインドの設定

--- Emacs/KeyBind.el
+++ Emacs/KeyBind.el
@@ -1,10 +1,43 @@
 
-;;========================================================================
-;;
-;;      KeyBind.el
-;;
-;;      Copyright (C) 2006-2014, Takahiro Itou.
-;;      All Rights Reserved.
-;;
-;;========================================================================
+;;========================================================================;;
+;;                                                                        ;;
+;;      KeyBind.el                                                        ;;
+;;                                                                        ;;
+;;      Copyright (C) 2006-2014, Takahiro Itou.                           ;;
+;;      All Rights Reserved.                                              ;;
+;;                                                                        ;;
+;;========================================================================;;
 
+(cond
+ ;;	Emacs on Cygwin.
+ ( (or (equal my-settings-editor "cygwin-emacs")
+       (equal my-settings-editor "cygwin-xemacs")
+   )
+   (setq  keyname-End           [select])
+   (setq  keyname-Home          [Home])
+   (setq  keyname-PageDown      [PageDown])
+   (setq  keyname-PageUp        [PageUp])
+   (setq  keyname-BackSpace     [BackSpace])
+   (setq  keyname-Delete        [Delete])
+   )
+ ;;	Other.
+ ( t
+   (setq  keyname-End           [End])
+   (setq  keyname-Home          [Home])
+   (setq  keyname-PageDown      [PageDown])
+   (setq  keyname-PageUp        [PageUp])
+   (setq  keyname-BackSpace     [BackSpace])
+   (setq  keyname-Delete        [Delete])
+   )
+)
+
+;;====================================================================;;
+;;                                                                    ;;
+;;    Move Cursor Settings.                                           ;;
+;;                                                                    ;;
+;;====================================================================;;
+
+(global-set-key  keyname-End         'move-end-of-line)
+(global-set-key  keyname-Home        'move-beginning-of-line)
+(global-set-key  keyname-PageDown    'up-a-log)
+(global-set-key  keyname-PageUp      'down-a-lot)
