f6f1ff54f049b67ced453f421028bfc35f3bb6c9
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2018-04-01 20:31:42 +0900

    シェル設定の準備
    (1) シェル設定用のファイルを更新

---
+++ Bash/BashProfile.sh
@@ -0,0 +1,7 @@
+
+if [ -f ~/Settings/Bash/Bashrc.sh ] ; then
+  source  ~/Settings/Bash/Bashrc.sh
+fi
+echo  "Load  ${HOME}/Settings/Bash/BashProfile.sh : OK"  1>&2
+
+
---
+++ Bash/Bashrc.sh
@@ -0,0 +1,7 @@
+
+if [ -f ~/Settings/Bash/BashAlias.sh ] ; then
+  source  ~/Settings/Bash/BashAlias.sh
+fi
+echo  "Load  ${HOME}/Settings/Bash/Bashrc.sh : OK"  1>&2
+
+
