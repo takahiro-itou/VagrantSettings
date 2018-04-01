45be63f14dfd40041a8c60272274c92027e8fbee
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2018-04-01 20:36:17 +0900

    設定を追加
    (1) プロンプトの設定を追加

--- Bash/Bashrc.sh
+++ Bash/Bashrc.sh
@@ -1,7 +1,13 @@
 
+if [ -f ~/Settings/Bash/Prompt.sh ] ; then
+  source  ~/Settings/Bash/Prompt.sh
+fi
+
 if [ -f ~/Settings/Bash/BashAlias.sh ] ; then
   source  ~/Settings/Bash/BashAlias.sh
 fi
-echo  "Load  ${HOME}/Settings/Bash/Bashrc.sh : OK"  1>&2
 
+if [ "x${MYBASH_ECHO_LOADING_BASHRC}y" = "xTRUEy" ] ; then
+  echo  "Load  ${HOME}/Settings/Bash/Bashrc.sh : OK"  1>&2
+fi
 
