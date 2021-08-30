35278b8f0385d513d55a8a6f359323e7e3218e04
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-08-30 14:49:41 +0900

    パッケージをインストール

---
+++ Emacs/InstallPackages.el
@@ -0,0 +1,11 @@
+
+;;====================================================================;;
+;;                                                                    ;;
+;;    Install Packages.                                               ;;
+;;                                                                    ;;
+;;====================================================================;;
+
+;;  package
+(require  'package)
+(add-to-list  'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
