60849f414bc06889d8fd56cb67cbee6ab2eea70c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-10-23 20:11:40 +0900

    プロンプトの設定を変更

--- Bash/Prompt.sh
+++ Bash/Prompt.sh
@@ -7,7 +7,7 @@ function get_hg_branch {
     HGBR=$(hg branch 2> /dev/null) && echo "hg[${HGBR}]"
 }
 
-PS1_BASE='\n\[\033[42m\][\t \d \u@\h \w]\[\033[0m\] '
+PS1_BASE='\n\[\033[46m\][\t \d \u@\h \w]\[\033[0m\] '
 PS1_BRANCH="\$(get_git_branch)"
 
 if [ "x${MYBASH_PROMPT_HG_BRANCH}y" = "xTRUEy" ] ; then
