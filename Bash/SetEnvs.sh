24760f307776a5b00c2025d2afafc613e0906758
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-07 20:32:25 +0900

    別リポジトリの作業内容を取り込み
    (1) ヒストリに記録しないコマンドを調整

--- Bash/SetEnvs.sh
+++ Bash/SetEnvs.sh
@@ -2,7 +2,7 @@
 export  LANG='ja_JP.UTF-8'
 
 export  HISTCONTROL=ignoreboth
-export  HISTIGNORE='fg*:bg*:history :history:cd:ls'
+export  HISTIGNORE='bg*:history :history:cd:ls'
 export  HISTTIMEFORMAT='%Y%m%d %T '
 
 test  -t 0  &&  stty    eof  ^P
